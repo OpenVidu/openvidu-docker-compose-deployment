fdc16ff4a361eb9cee95f73a11b92ff62afb9bf3
cruizba <carlos.ruizbal@gmail.com>
2026-03-04 11:55:08 +0100

    Open Source Community Caddy and use it in docker-compose.yaml

--- docker/caddy-proxy/entrypoint.sh
+++ docker/caddy-proxy/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 
-if [ -z "$DOMAIN_NAME" ]; then
-    echo "ERROR: DOMAIN_NAME environment variable is not set" >&2
-    exit 1
+# If non opinionated compose mode, launch with entrypoint
+if [ "$NON_OPINIONATED_COMPOSE_MODE" = "true" ]; then
+    if [ -z "$DOMAIN_NAME" ]; then
+        echo "ERROR: DOMAIN_NAME environment variable is not set" >&2
+        exit 1
+    fi
+    sed "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" /etc/caddy/caddy.yaml > /tmp/caddy-resolved.yaml
+
+    exec caddy run --config /tmp/caddy-resolved.yaml --adapter yaml
 fi
 
-sed "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" /etc/caddy/caddy.yaml > /tmp/caddy-resolved.yaml
-
-exec caddy run --config /tmp/caddy-resolved.yaml --adapter yaml
+# Otherwise, launch with command line arguments (Community Single Node)
+exec caddy "$@"
